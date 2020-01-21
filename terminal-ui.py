006068202e12e92f48234583192e8309e076b526
RunningPixEl <kangkenan1@163.com>
2020-01-21 23:04:57 +0800

    Update terminal-ui.py

--- terminal-ui.py
+++ terminal-ui.py
@@ -1,1 +1,15 @@
 # reference: https://github.com/gojuukaze/terminal_layout
+# https://terminal-layout.readthedocs.io/en/latest/
+import time
+from terminal_layout import *
+
+ctl = LayoutCtl(TextView('id1', 'hello world!', width=20, fore=Fore.red, back=Back.green))
+
+ctl.draw()
+
+time.sleep(2)
+
+view = ctl.find_view_by_id('id1')
+view.text = 'hi world'
+ctl.re_draw()
+# Success install in jetson nano, use python3 to run it
